25a83c08cb9be972dfeffcc34b536c226d59f9eb
tejasvi.s <tejasvi.s@vtiger.com>
2021-05-28 17:38:19 +0530

    deploy-test-03

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 STATIC_DIR = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
