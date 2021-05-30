56420f2086ecd16d11c767172db25ff5540d7121
kushtej <007kushtej@gmail.com>
2021-05-30 19:44:53 +0530

    Please work

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -123,7 +123,8 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-if DEBUG == True:
-        STATICFILES_DIRS=[
-            os.path.join(BASE_DIR,"static")
-    ]
+
+
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,"static")
+# ]
