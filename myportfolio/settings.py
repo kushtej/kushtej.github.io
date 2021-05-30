eddfc32345bd40edce303a5ef49a4251f5a44db0
kushtej <007kushtej@gmail.com>
2021-05-30 19:32:18 +0530

    check this

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -123,7 +122,9 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-if not DEBUG:
+if DEBUG == True:
         STATICFILES_DIRS=[
             os.path.join(BASE_DIR,"static")
     ]
+else :
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
