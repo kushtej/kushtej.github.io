e7a90bf489e441c20e0bea19e9eb2087a1a86930
kushtej <007kushtej@gmail.com>
2021-05-30 14:35:01 +0530

    Added static for dev and production mode

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+if DEBUG == True:
+        STATICFILES_DIRS=[
+            os.path.join(BASE_DIR,"static")
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
