1272a2a84e9ce186d1a99de27609eed4d2dfcc58
tejasvi.s <tejasvi.s@vtiger.com>
2021-06-20 11:31:05 +0530

    added preview of inquery portal

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -51,7 +51,7 @@ var data = {
         {
             id:"p-02",
             title: "Inquiry Portal",
-            media: {type:"Img",content:"https://github.com/kushtej/nodejs-inquiry-portal/blob/635612e6a051e42c6318306731596419c0eba828/preview/preivew.gif"},
+            media: {type:"Img",content:"app/images/projects/p-02.gif"},
             subtitle: "A Realtime Inquiry portal to chat with clients/Agents in realtime and ask any inquiry related to anything.",
             discription:"A LSTM (RNN) based Deep learning model trained for kannada language to suggest the next words in a sentence for the given text powered by flask framework. The project is built as a platform where a user can signup/login by email verification, create a new file in the editor,save etc.",
             link: "https://github.com/kushtej/nodejs-inquiry-portal"
